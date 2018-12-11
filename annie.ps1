d81065ed9dc667dd58946f6f1c805b94e3ef09b2
Franklin Chen <myfreedom614@gmail.com>
2018-12-11 14:28:12 +0800

    Update annie

--- annie.ps1
+++ annie.ps1
@@ -1,6 +1,5 @@
 ﻿param (
     [string]$url = $( Read-Host "Input bilibili url" )
  )
-chcp 65001 | Out-Null 
 $OutputVariable = .\annie.exe -i -c cookies.txt $url | out-string
 Write-Host $OutputVariable
